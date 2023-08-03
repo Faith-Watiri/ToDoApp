9f25d949b6122e0369cca1fea0c89b6f76c19eb2
Amos Nyaburi <mogaka.amo254@gmail.com>
2023-08-03 12:38:54 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,5 @@ const babelConfig = require("./todoapp/babel.config")
 
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-   plugins: [nativewind/babelConfig]
+   plugins: ['nativewind/babelConfig']
 };
